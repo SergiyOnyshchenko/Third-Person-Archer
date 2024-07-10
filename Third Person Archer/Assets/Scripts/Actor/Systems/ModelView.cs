b8080ceb64c5e9f1c7e9e0e6ea4be22b6331ad8d
goldenitsir <golden.hack.non@gmail.com>
2024-07-10 13:35:29 +0300

    Revert "bug fixes"
    This reverts commit 90960d345e65149946b8abc6bb7c356ced4eefe1.

--- Third Person Archer/Assets/Scripts/Actor/Systems/ModelView.cs
+++ Third Person Archer/Assets/Scripts/Actor/Systems/ModelView.cs
@@ -21,7 +21,7 @@ namespace Actor
                 return;
 
             if (value)
-                _outline.OutlineWidth = 0f;
+                _outline.OutlineWidth = 1f;
             else
                 _outline.OutlineWidth = 0;
         }
