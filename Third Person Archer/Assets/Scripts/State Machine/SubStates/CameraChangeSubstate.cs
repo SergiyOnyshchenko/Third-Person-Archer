6861d8095dd9475dd594a81e1eaea37d82aea6d3
SergiyOnyshchenko <87852991+SergiyOnyshchenko@users.noreply.github.com>
2025-05-28 15:58:52 +0300

    landscape view

--- Third Person Archer/Assets/Scripts/State Machine/SubStates/CameraChangeSubstate.cs
+++ Third Person Archer/Assets/Scripts/State Machine/SubStates/CameraChangeSubstate.cs
@@ -6,11 +6,13 @@ using Cinemachine;
 public class CameraChangeSubstate : SubState
 {
     [SerializeField] private CinemachineVirtualCamera _camera;
+    private float _fovMultiplier = 0.5f;
 
     public override void Enter()
     {
         base.Enter();
         _camera.Priority = 2;
+        _camera.m_Lens.FieldOfView = _camera.m_Lens.FieldOfView * _fovMultiplier;
     }
 
     public override void Exit()
